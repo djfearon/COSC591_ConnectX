ec1467f4abc3c1d4ea151d5f60be2f2970b4b078
nareshkudithi <nkudithi0@frostburg.edu>
2017-05-01 17:29:40 -0400

    creating the pattern

---
+++ src/test/java/edu/frostburg/cosc591_connectx/ConnectFour.java
@@ -0,0 +1,52 @@
+package getopt;
+
+
+
+public class ConnectFour
+{
+public static String[][] createPattern()
+{
+ 
+   String[][] f = new String[7][15];
+  
+  for (int i =0;i<f.length;i++)
+  {
+    
+     for (int j =0;j<f[i].length;j++)
+    {
+   
+      if (j% 2 == 0) f[i][j] ="|";
+      else f[i][j] = " ";
+      
+      if (i==6) f[i][j]= "-";
+    }
+    
+  }
+  return f;
+}
+
+public static void printPattern(String[][] f)
+{
+  for (int i =0;i<f.length;i++)
+  {
+    for (int j=0;j<f[i].length;j++)
+    {
+      System.out.print(f[i][j]);
+    }
+    System.out.println();
+  }
+}
+
+
+
+public static void main (String[] args)
+{
+
+  String[][] f = createPattern();
+
+  boolean loop = true;
+  int count = 0;
+  printPattern(f);
+ 
+}
+}
